25426e493bf216d855dadf0090afac5b1ff42c4c
muhamadiyarov-ar <115489747+muhamadiyarov-ar@users.noreply.github.com>
2022-10-19 14:19:41 +0300

    Лабораторная работа №3 (Дополнил)

---
+++ test_03.py
@@ -0,0 +1,14 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0  # количество месяцев, которое можно прожить
+
+# TODO Оформить решение
+while money_capital >= spend:
+    money_capital = money_capital + salary - spend
+    spend *= 1.05
+    month += 1
+
+print(month)
