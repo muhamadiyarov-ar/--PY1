b1e16a4480e8bbbb5a8c13a934fb0a096e9ffc1f
muhamadiyarov-ar <115489747+muhamadiyarov-ar@users.noreply.github.com>
2022-10-20 17:51:36 +0300

    Delete test_01.py

--- test_01.py
+++
@@ -1,9 +0,0 @@
-src = not False and True or False and not True
-
-
-result = True and True or False and False
-result = (True and True) or (False and False)
-result = True or False
-result = True
-
-print(src == result)
