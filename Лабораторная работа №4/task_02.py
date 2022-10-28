12f7eecc7ef33a67b4fb7a8c2a08788c53c47d10
muhamadiyarov-ar <115489747+muhamadiyarov-ar@users.noreply.github.com>
2022-10-28 07:39:14 +0300

    Лабораторная работа №4

---
+++ Лабораторная работа №4/task_02.py
@@ -0,0 +1,20 @@
+def get_count_char(str_):
+    abc_dict = {}
+    str_ = "".join(str_.split())
+    str_ = str_.lower()
+
+    for sign in str_:
+        if sign.isalpha():
+            if sign in abc_dict:
+                abc_dict[sign] += 1
+            else:
+                abc_dict[sign] = 1
+
+    return abc_dict
+
+main_str = """
+    Данное предложение будет разбиваться на отдельные слова. 
+    В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
+    Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
+"""
+print(get_count_char(main_str))
