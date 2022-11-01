3de16f4e915ddb116e609776db6fb3c07e9c7821
muhamadiyarov-ar <115489747+muhamadiyarov-ar@users.noreply.github.com>
2022-11-01 15:02:09 +0300

    Update task_02.py

--- Лабораторная работа №4/task_02.py
+++ Лабораторная работа №4/task_02.py
@@ -18,3 +18,15 @@ main_str = """
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
 print(get_count_char(main_str))
+
+abc_dict = get_count_char(main_str)
+
+def get_symbols(abc_dict):
+    total_count = sum(abc_dict.values())
+    new_dict = {}
+    for count in abc_dict:
+        new_dict[count] = abc_dict[count]/total_count * 100
+    return new_dict
+
+result = get_symbols(abc_dict)
+print(result)
